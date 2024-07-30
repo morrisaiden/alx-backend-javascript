04c96f5739ef476da370bc5df9d91c802f7e9155
morrisaiden <mwaimorris1997@gmail.com>
2024-07-30 19:14:46 +0300

    Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentByLocation(students, city) {
- return students.filter((student) => student.location === city);
+export default function getStudentByLocation (students, city) {
+  return students.filter((student) => student.location === city);
 }
