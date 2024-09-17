47d0aef15797951f522826783fd7d4590ef66b31
morrisaiden <mwaimorris1997@gmail.com>
2024-09-17 13:13:25 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,7 +5,7 @@ const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     try {
       const output = await countStudents('database.csv');
