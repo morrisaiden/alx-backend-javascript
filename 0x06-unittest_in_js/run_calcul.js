c25468151d646ff77a5aa04f6f6f68f54e38ceb4
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2024-09-19 11:32:18 +0300

    Delete 0x06-unittest_in_js directory

--- 0x06-unittest_in_js/run_calcul.js
+++
@@ -1,6 +0,0 @@
-const calculateNumber = require('./0-calcul');
-
-console.log(calculateNumber(1, 3));
-console.log(calculateNumber(1, 3.7));
-console.log(calculateNumber(1.2, 3.7));
-console.log(calculateNumber(1.5, 3.7));
