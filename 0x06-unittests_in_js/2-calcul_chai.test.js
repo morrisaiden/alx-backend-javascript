fb3c13ec199fa8c49fa5cea77e3fc731c69bbd70
morrisaiden <mwaimorris1997@gmail.com>
2024-09-20 13:02:38 +0300

    Chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,7 @@
 
-const chai = require('chai');
-const calculateNumber = require('./2-calcul_chai');
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
+const calculateNumber = require("./2-calcul_chai");
 
 describe('calculateNumber', function () {
   describe('SUM', function () {
@@ -9,31 +10,31 @@ describe('calculateNumber', function () {
     });
 
     it('should return 0 when inputs are -1.4 and 1.4', function () {
-      chai.expect(calculateNumber('SUM', -1.4, 1.4), 0);
+      expect(calculateNumber('SUM', -1.4, 1.4), 0);
     });
   });
 
   describe('SUBTRACT', function () {
     it('should return -4 when inputs are 1.4 and 4.5', function () {
-      chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+     expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
 
     it('should return 0 when inputs are 1.4 and 1.4', function () {
-      chai.expect(calculateNumber('SUBTRACT', 1.4, 1.4), 0);
+     expect(calculateNumber('SUBTRACT', 1.4, 1.4), 0);
     });
   });
 
   describe('DIVIDE', function () {
     it('should return 0.2 when inputs are 1.4 and 4.5', function () {
-      chai.expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+     expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
     it('should return "Error" when dividing by 0', function () {
-      chai.expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+     expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 
     it('should return -0.5 when dividing -1.5 by 1.5', function () {
-      chai.expect(calculateNumber('DIVIDE', -1.5, 1.5), -0.5);
+     expect(calculateNumber('DIVIDE', -1.5, 1.5), -0.5);
     });
   });
 });
